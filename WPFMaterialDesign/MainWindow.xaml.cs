8c56f3a3779aceb02d80224a4e9a41ecd711cea8
кирилл Захаров <csgo2003@mail.ru>
2019-04-29 18:07:42 +0600

    first commit

---
+++ WPFMaterialDesign/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMaterialDesign
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            firstNameLabel.Content = "Ruslan";
+            lastNameLabel.Content = "Shadikyan";
+            addressLabel.Content = "Samal 10";
+            birthdayDateLabel.Content = "15.01.2003";
+            groupLabel.Content = "SDP 181/1";
+        }
+    }
+}
